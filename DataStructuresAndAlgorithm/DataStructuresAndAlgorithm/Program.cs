79a960237b6dc43f33f33adf940085d5165c24e3
OyebisiJemil <oyebisijemil@gmail.com>
2018-05-27 15:45:18 -0400

    Reverse array and Max of the sum of the 6 by 6 array of hourglass

--- DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/Program.cs
@@ -12,10 +12,40 @@ namespace DataStructuresAndAlgorithm
         static void Main(string[] args)
         {
             ArrayProcessing arrayProcessing = new ArrayProcessing();
-            int[] arrayOfIntegers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int input = 0;
+          
+            Console.WriteLine("Welcome to Data Structure and Algorithm solution");
+            Console.WriteLine("Press 1 for Reversing array");
+            Console.WriteLine("Press 2 for Max of hourglass");
+            Console.WriteLine();//just to leave a line 
+            input= int.Parse(Console.ReadLine());
+            Console.WriteLine();//just to leave a line 
+            switch (input)
+            {
+                case 1:
+                     int[] arrayOfIntegers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                     arrayProcessing.ReverseArrayInASingleSpaceSeparatedIntegers(arrayOfIntegers);
+                     break;
+                case 2:
+                     int[][] array = new int[6][] {
+                                                    new int[] {1, 1, 1, 0, 0, 0},
+                                                    new int[] {0, 1, 0, 0, 0, 0},
+                                                    new int[] {1, 1, 1, 0, 0, 0},
+                                                    new int[] {0, 0, 2, 4, 4, 0},
+                                                    new int[] {0, 0, 0, 2, 0, 0},
+                                                    new int[] {0, 0, 1, 2, 4, 0}
+                                                  };
 
-            arrayProcessing.ReverseArrayInASingleSpaceSeparatedIntegers(arrayOfIntegers);
 
+
+
+
+                     arrayProcessing.MaxOfTheSumOfHourGlass(array);
+                     break;
+                default:
+                     Console.WriteLine("Press a valid key between 1 and 2");
+                     break;
+            }
             Console.ReadLine();
         }
     }
